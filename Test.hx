b1ca9333eae39cbe059c2d7ad04b19378d24c24c
underscorediscovery <sven@underscorediscovery.com>
2016-12-02 13:22:22 -0330

    initial commit

---
+++ Test.hx
@@ -0,0 +1,24 @@
+class Test {
+    
+
+    static function main() {
+
+        var bst: BalancedBST<Null<Int>, String> = new BalancedBST(function(keyA, keyB) {
+            if(keyA < keyB) return -1;
+            if(keyA > keyB) return 1;
+            return 0;
+        });
+
+        bst.insert(6, 'six');
+        bst.insert(4, 'four');
+        bst.insert(2, 'two');
+        bst.insert(1, 'one');
+        bst.insert(3, 'three');
+        bst.insert(5, 'five');
+
+        for(string in bst) {
+            trace(string);
+        }
+
+    }
+}
